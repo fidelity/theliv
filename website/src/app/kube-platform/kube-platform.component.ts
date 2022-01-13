abfb21d1c895b92826062014893acfef96fdbe5e
Zhang, Alan (TEC) <yang.zhang2@fmr.com>
2022-01-13 15:42:48 +0800

    fix ui issue

--- website/src/app/kube-platform/kube-platform.component.ts
+++ website/src/app/kube-platform/kube-platform.component.ts
@@ -43,6 +43,7 @@ export class KubePlatformComponent implements OnInit {
   clusterFormControl = new FormControl();
   clusterOptions: Observable<string[]> | undefined;
   namespaces: NamespaceOption[] = []
+  clusterInputing = false
 
   constructor(
     private kubeService: KubernetesService,
@@ -215,6 +216,7 @@ export class KubePlatformComponent implements OnInit {
   }
 
   checkClusterBlank(){
+    this.clusterInputing = true
     if (this.selectedClusters == '') {
       this.selectedNs = ''
       this.namespaces = []
@@ -225,12 +227,13 @@ export class KubePlatformComponent implements OnInit {
   }
 
   getNSByCluster(): void {
-    this.router.navigate(['kubernetes']);
-    this.resourceGroups = [];
-    this.selectedNs = '';
-    this.kubeService.selectedNs$.next(this.selectedNs);
-    this.kubeService.resourceList$.next(this.resourceGroups);
-    this.getNamespaces()
+    this.clusterInputing = false
+    this.router.navigate(['kubernetes'], { queryParams: { cluster: this.selectedClusters } });
+    // this.resourceGroups = [];
+    // this.selectedNs = '';
+    // this.kubeService.selectedNs$.next(this.selectedNs);
+    // this.kubeService.resourceList$.next(this.resourceGroups);
+    // this.getNamespaces()
   }
 
   getNamespaces(): void {
@@ -252,13 +255,13 @@ export class KubePlatformComponent implements OnInit {
   }
 
   getSelectedQuery(e: any): void {
-    this.router.navigate(['kubernetes']);
-    this.resourceGroups = [];
-    this.kubeService.selectedClusters$.next(this.selectedClusters);
-    this.kubeService.selectedNs$.next(this.selectedNs);
-    if (this.selectedClusters && this.selectedNs) {
-      this.getKubeResourceInfo();
-    }
+    this.router.navigate(['kubernetes'], { queryParams: { cluster: this.selectedClusters, namespace: e.value } });
+    // this.resourceGroups = [];
+    // this.kubeService.selectedClusters$.next(this.selectedClusters);
+    // this.kubeService.selectedNs$.next(this.selectedNs);
+    // if (this.selectedClusters && this.selectedNs) {
+    //   this.getKubeResourceInfo();
+    // }
   }
 
 }
