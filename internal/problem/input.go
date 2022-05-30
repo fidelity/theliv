370e803465a81ce9371ac82ab1411fdf49085e2a
Zhao, Olivia <xueming.zhao@fmr.com>
2022-05-30 13:26:21 +0800

    code clean up for new base

--- internal/problem/input.go
+++ internal/problem/input.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/fidelity/theliv/pkg/observability"
 	"k8s.io/client-go/rest"
 )
 
@@ -19,15 +18,15 @@ type TimeSpan struct {
 }
 
 type DetectorCreationInput struct {
-	LogRetriever           observability.LogRetriever
-	EventRetriever         observability.EventRetriever
-	LogDeeplinkRetriever   observability.LogDeeplinkRetriever
-	EventDeeplinkRetriever observability.EventDeeplinkRetriever
-	Kubeconfig             *rest.Config
-	Namespace              string
-	ClusterName            string
-	Platform               SupportedPlatform
-	EventTimespan          TimeSpan
-	LogTimespan            TimeSpan
-	AwsConfig              aws.Config
+	// LogRetriever           observability.LogRetriever
+	// EventRetriever         observability.EventRetriever
+	// LogDeeplinkRetriever   observability.LogDeeplinkRetriever
+	// EventDeeplinkRetriever observability.EventDeeplinkRetriever
+	Kubeconfig  *rest.Config
+	Namespace   string
+	ClusterName string
+	// Platform               SupportedPlatform
+	EventTimespan TimeSpan
+	LogTimespan   TimeSpan
+	AwsConfig     aws.Config
 }
