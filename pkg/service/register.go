f8e0b2c3aafb6217b0e8f817350454924ee2aa13
padraigmc <Padraig.McCarthy@fmr.com>
2023-08-08 04:51:58 -0400

    feat: convert basic info to json by go template, added logging
    Signed-off-by: padraigmc <Padraig.McCarthy@fmr.com>

--- pkg/service/register.go
+++ pkg/service/register.go
@@ -7,16 +7,18 @@ package service
 
 import (
 	"context"
-	"encoding/json"
+	"strings"
 
 	invest "github.com/fidelity/theliv/internal/investigators"
 	"github.com/fidelity/theliv/pkg/database/etcd"
+	log "github.com/fidelity/theliv/pkg/log"
+	"go.uber.org/zap"
 )
 
 const (
-	KeyPath  = "/theliv/clusters/"
-	KubeConf = "/kubeconf"
-	BasicKey = "/basic"
+	KeyPath     = "/theliv/clusters/"
+	KubeConfKey = "/kubeconf"
+	BasicKey    = "/basic"
 
 	TokenTemplate = `apiVersion: v1
 clusters:
@@ -36,42 +38,57 @@ users:
 - name: {{.Name}}
   user:
     token: {{.Token}}`
+
+	BasicTemplate = `{
+	"provider": "aws",
+	"account": "{{.Account}}",
+	"role": "arn:aws:iam::{{.Account}}:role/EKS_Theliv",
+	"name": "{{.Name}}",
+	"region": "{{.Region}}"
+}`
 )
 
 type ClusterBasic struct {
-	Url   string
-	CA    string
-	Name  string
-	Token string
-	AWS   AWS
-}
-
-type AWS struct {
-	Account string `json:"account"`
+	Url     string
+	CA      string
+	Name    string
+	Token   string
+	Account string
+	Region  string
 }
 
 // Insert or update 1 record, to /theliv/clusters/{name}/kubeconf.
 func RegisterCluster(ctx context.Context, basic ClusterBasic) error {
-
 	clusterType := basic.Name[:3]
+	etcdBaseKey := KeyPath + clusterType + "/" + basic.Name
+
+	l := log.SWithContext(ctx).With(
+		zap.String("cluster", basic.Name),
+	)
+	l.Info("Registering cluster with Theliv")
+
+	// if aws account id present, convert to json and insert in db
+	if basic.Account != "" {
+		l.Infof("AWS config found with account ID: %s", basic.Account)
+		if urlSlice := strings.Split(basic.Url, "."); len(urlSlice) > 5 {
+			basic.Region = urlSlice[len(urlSlice)-4]
+		}
+
+		basicJson, err := invest.ExecGoTemplate(ctx, BasicTemplate, basic)
+		if err != nil {
+			return err
+		}
+
+		if err := etcd.PutStr(etcdBaseKey+BasicKey, basicJson); err != nil {
+			return err
+		}
+	}
 
 	value, err := invest.ExecGoTemplate(ctx, TokenTemplate, basic)
 	if err != nil {
 		return err
 	}
 
-	// if aws account id present, convert to json and insert in db
-	if basic.AWS != (AWS{}) {
-		basicJson, err := json.Marshal(basic.AWS)
-		if err != nil {
-			return err
-		}
-
-		if err := etcd.PutStr(KeyPath+clusterType+"/"+basic.Name+BasicKey, string(basicJson)); err != nil {
-			return err
-		}
-	}
-
-	return etcd.PutStr(KeyPath+clusterType+"/"+basic.Name+KubeConf, value)
+	return etcd.PutStr(etcdBaseKey+KubeConfKey, value)
 
 }
