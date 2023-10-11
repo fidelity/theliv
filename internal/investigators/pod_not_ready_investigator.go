c6f4800afe7393cc3b6af7dae43e16527913fb47
padraigmc <Padraig.McCarthy@fmr.com>
2023-10-11 03:56:37 -0400

    refactored pod_not_ready

--- internal/investigators/pod_not_ready_investigator.go
+++ internal/investigators/pod_not_ready_investigator.go
@@ -13,19 +13,21 @@ import (
 
 	"github.com/fidelity/theliv/internal/problem"
 	log "github.com/fidelity/theliv/pkg/log"
+	"github.com/fidelity/theliv/pkg/observability"
 	v1 "k8s.io/api/core/v1"
 )
 
 const (
 	ReadinessProbeFailedSolution = `1. Pod has status 'ContainersNotReady'.
 2. It appears there is an issue with your readiness probe.
-3. {{.}}.
+3. Events:{{if not .}} no events found.{{else}}{{range .}}
+- {{.}}.{{end}}{{end}}
 4. For more details, please refer to the Events section. Click on the pod name above to see the Pod configuration.
 `
 
 	ReadinessGateFailedSolution = `1. Pod has status 'ReadinessGatesNotReady'.
 2. It appears there is an issue with your readiness gates.
-3. Message: {{.}}.
+3. Message: {{if not .}}no message found{{else}}{{.}}.{{end}}
 4. Please check the readiness gate configurations for your pod. Click on the pod name above to see the Pod configurations.
 `
 
@@ -61,21 +63,30 @@ func PodNotReadyInvestigator(ctx context.Context, wg *sync.WaitGroup, problem *p
 					log.SWithContext(ctx).Error("Got error when calling Kubernetes event API, error is %s", err)
 				}
 
-				msg := "Message: " + con.Message
+				var msg []string
+				if con.Message != "" {
+					msg = []string{con.Message}
+				} else {
+					msg = []string{}
+				}
 				if len(events) > 0 {
-					for _, event := range events {
-						for _, eventMsg := range PodNotReadyEventMessage {
-							matched, err := regexp.MatchString(strings.ToLower(eventMsg), strings.ToLower(event.Message))
-							if err == nil && matched {
-								log.SWithContext(ctx).Infof("Found event with error '%s', pod %s", eventMsg, pod.Name)
-								msg = "Event: " + event.Message
-							}
-						}
-					}
+					getMsgFromEvents(ctx, events, pod, &msg)
 				}
 				solution = GetSolutionsByTemplate(ctx, ReadinessProbeFailedSolution, msg, true)
 			}
 			appendSolution(problem, solution, GetSolutionsByTemplate(ctx, UsefulCommands, pod, true))
+		}
+	}
+}
+
+func getMsgFromEvents(ctx context.Context, events []observability.EventRecord, pod v1.Pod, msg *[]string) {
+	for _, event := range events {
+		for _, eventMsg := range PodNotReadyEventMessage {
+			matched, err := regexp.MatchString(strings.ToLower(eventMsg), strings.ToLower(event.Message))
+			if err == nil && matched {
+				log.SWithContext(ctx).Infof("Found event with error '%s', pod %s", eventMsg, pod.Name)
+				*msg = append(*msg, event.Message)
+			}
 		}
 	}
 }
